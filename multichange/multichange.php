a24f3fe1079ce1fe5417b8f718c8b188a05dc934
Peterjah <petterseznec@gmail.com>
2018-01-30 18:02:47 +0100

    testing on 4 crypto

--- multichange/multichange.php
+++ multichange/multichange.php
@@ -34,45 +34,52 @@ while(true)
   //EUR -> BTC -> LSK -> EUR
   //Get order book
 
-  $BTC_EURbook = getOrderBook($abucoinsApi, 'BTC-EUR');
-  $LSK_BTCbook = getOrderBook($abucoinsApi, 'LSK-BTC');
-  $LSK_EURbook = getOrderBook($abucoinsApi, 'LSK-EUR');
-
-
-  $bestBTC_Seller = $BTC_EURbook['asks']; //var_dump($bestBTC_Seller);
-  $bestLSK_Seller = $LSK_BTCbook['asks']; //var_dump($bestLSK_Seller);
-  $bestLSK_Buyer = $LSK_EURbook['bids']; //var_dump($bestLSK_Buyer);
-
-  $roundAmountEur = $bestBTC_Seller['price'] * $bestBTC_Seller['size'];
-  if( ($bestLSK_Seller['price'] *$bestLSK_Seller['size']) < $bestBTC_Seller['size'])
+  foreach(['LSK', 'ETH', 'BCH' , 'BTG'] as $Crypto)
   {
-    $btc2buy = $bestLSK_Seller['price'] *$bestLSK_Seller['size'];
-    $roundAmountEur = $btc2buy * $bestBTC_Seller['price'];
-  }
-  if($bestLSK_Buyer['price'] * $bestLSK_Buyer['size'] < $roundAmountEur)
-  {
-    $roundAmountEur = $bestLSK_Buyer['price'] * $bestLSK_Buyer['size'];
-  }
-
-  if($roundAmountEur >= $balance)
-    $roundAmountEur = $balance;
-  var_dump("play with = $roundAmountEur EUR");
-
-  //buy $roundAmountEur of btc
-  $tmpBtc = $roundAmountEur / $bestBTC_Seller['price'];
-    //var_dump("tmpBtc = $tmpBtc");
-  //buy $tmpBtc of lsk
-  $tmpLsk = $tmpBtc / $bestLSK_Seller['price'];
-    //var_dump("tmpLsk = $tmpLsk");
-  //sell lsk for eur
-  $gainEur = $tmpLsk * $bestLSK_Buyer['price'] - $roundAmountEur;
-
-  print("gain = $gainEur\n");
+    print("Try multi switch with $Crypto\n");
+    $BTC_EURbook = getOrderBook($abucoinsApi, 'BTC-EUR');
+    $Crypto_BTCbook = getOrderBook($abucoinsApi, "$Crypto-BTC");
+    $Crypto_EURbook = getOrderBook($abucoinsApi, "$Crypto-EUR");
 
 
-  if($gainEur > 0.1) //10 cts!
-  { // do trades
+    $bestBTC_Seller = $BTC_EURbook['asks']; //var_dump($bestBTC_Seller);
+    $bestLSK_Seller = $Crypto_BTCbook['asks']; //var_dump($bestLSK_Seller);
+    $bestLSK_Buyer = $Crypto_EURbook['bids']; //var_dump($bestLSK_Buyer);
 
+    $roundAmountEur = $bestBTC_Seller['price'] * $bestBTC_Seller['size'];
+    if( ($bestLSK_Seller['price'] *$bestLSK_Seller['size']) < $bestBTC_Seller['size'])
+    {
+      $btc2buy = $bestLSK_Seller['price'] *$bestLSK_Seller['size'];
+      $roundAmountEur = $btc2buy * $bestBTC_Seller['price'];
+      //print("wesh1 $roundAmountEur\n");
+    }
+    if($bestLSK_Buyer['price'] * $bestLSK_Buyer['size'] < $roundAmountEur)
+    {
+      $roundAmountEur = $bestLSK_Buyer['price'] * $bestLSK_Buyer['size'];
+      //print("wesh2 $roundAmountEur\n");
+    }
+
+//    if($roundAmountEur >= $balance)
+//      $roundAmountEur = $balance;
+    print("play with = $roundAmountEur EUR\n");
+
+    //buy $roundAmountEur of btc
+    $tmpBtc = $roundAmountEur / $bestBTC_Seller['price'];
+      //var_dump("tmpBtc = $tmpBtc");
+    //buy $tmpBtc of lsk
+    $tmpLsk = $tmpBtc / $bestLSK_Seller['price'];
+      //var_dump("tmpLsk = $tmpLsk");
+    //sell lsk for eur
+    $gainEur = $tmpLsk * $bestLSK_Buyer['price'] - $roundAmountEur;
+
+    print("gain = ".number_format($gainEur,2)." Eur ".number_format(($gainEur/$roundAmountEur)*100, 2) ."%\n");
+
+
+    if($gainEur > 0.1) //10 cts!
+    { // do trades
+      exec("echo $gainEur >> gains")
+    }
+    print("\n");
   }
   print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
   sleep(2);
