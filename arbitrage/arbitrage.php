e87f7f48801f96aee1ccf9b1f50a3888255da8ab
Peterjah <petterseznec@gmail.com>
2018-02-20 19:27:31 +0100

    add functionnality for arbitrage bot

--- arbitrage/arbitrage.php
+++ arbitrage/arbitrage.php
@@ -6,41 +6,82 @@ $keys = json_decode(file_get_contents("../common/private.keys"));
 $abucoinsApi = new AbucoinsApi($keys->abucoins);
 $CryptopiaApi = new CryptopiaApi($keys->cryptopia);
 
-
+$profit = 0;
 while(true)
 {
-  foreach( ['GNT' ,'HSR', 'LTC', 'XMR', 'STRAT', 'ETC', 'TRX', 'ETH', 'ARK', 'BCH', 'REP'] as $Alt)
+  foreach( ['GNT' ,'HSR', 'LTC', 'XMR', 'STRAT', 'ETC', 'TRX', 'ETH', 'ARK', 'BCH', 'REP'] as $alt)
   {
-    //Get order book
-    $AbuOrderbook = new OrderBook($abucoinsApi, "$Alt-BTC");
+    $AbuOrderbook = new OrderBook($abucoinsApi, "$alt-BTC");
     $abuBook = $AbuOrderbook->book;
-    $CryptOrderbook = new OrderBook($CryptopiaApi, "$Alt-BTC");
+    $CryptOrderbook = new OrderBook($CryptopiaApi, "$alt-BTC");
     $cryptBook = $CryptOrderbook->book;
 
-    print "buy {$abuBook['asks']['size']}$Alt on Abucoins at {$abuBook['asks']['price']}BTC\n";
-    print "sell {$cryptBook['bids']['size']}$Alt on Cryptopia at {$cryptBook['bids']['price']}BTC\n";
+    print "buy {$abuBook['asks']['size']}$alt on Abucoins at {$abuBook['asks']['price']}BTC\n";
+    print "sell {$cryptBook['bids']['size']}$alt on Cryptopia at {$cryptBook['bids']['price']}BTC\n";
 
     $gain_percent = (($cryptBook['bids']['price']/$abuBook['asks']['price'])-1)*100;
     print "gain $gain_percent%\n";
     $tradeSize = $cryptBook['bids']['size'] > $abuBook['asks']['size'] ? $abuBook['asks']['size'] : $cryptBook['bids']['size'];
     $gain1 = $tradeSize * ($cryptBook['bids']['price']*((100-$CryptOrderbook->fees)/100) - $abuBook['asks']['price']);
-    $gain1_str = "gain1: buy $tradeSize $Alt on abu sell cryptop: ".$gain1."BTC ($gain_percent%)\n";
+    $gain1_str = "gain1: buy $tradeSize $alt on abu sell cryptop: ".$gain1."BTC (".number_format($gain_percent,2)."%)\n";
     print "$gain1_str";
     if($gain1>0)
+    {
+      $profit+=$gain1;
       file_put_contents('gain1',$gain1_str,FILE_APPEND);
+    }
 
 
-    print "\nbuy {$cryptBook['asks']['size']}$Alt on Cryptopia at {$cryptBook['asks']['price']}BTC\n";
-    print "sell {$abuBook['bids']['size']}$Alt on Abucoins at {$abuBook['bids']['price']}BTC\n";
+    print "\nbuy {$cryptBook['asks']['size']}$alt on Cryptopia at {$cryptBook['asks']['price']}BTC\n";
+    print "sell {$abuBook['bids']['size']}$alt on Abucoins at {$abuBook['bids']['price']}BTC\n";
     $gain_percent = (($abuBook['bids']['price']/$cryptBook['asks']['price'])-1)*100;
     print "gain $gain_percent%\n";
     $tradeSize = $cryptBook['asks']['size'] > $abuBook['bids']['size'] ? $abuBook['bids']['size'] : $cryptBook['asks']['size'];
     $gain2 = $tradeSize * ($abuBook['bids']['price'] - $cryptBook['asks']['price']*((100+$CryptOrderbook->fees)/100));
-    $gain2_str = "gain2: buy $tradeSize $Alt on cryptopia sell abuc: ".$gain2."BTC ($gain_percent%)\n";
+    $gain2_str = "gain2: buy $tradeSize $alt on cryptopia sell abuc: ".$gain2."BTC (".number_format($gain_percent,2)."%)\n";
     print "$gain2_str\n";
     if($gain2>0)
+    {
+      $profit+=$gain2;
       file_put_contents('gain2',$gain2_str,FILE_APPEND);
+
+      //check both balances
+      $account = $abucoinsApi->jsonRequest('GET', "/accounts/10502694-$alt", null);
+      $balance = $account->available;
+      $btc_account = $CryptopiaApi->jsonRequest("GetBalance",['Currency'=> "BTC"]);
+      $btc_bal = $btc_account[0]->Available;
+
+      $btc_to_spend = $cryptBook['asks']['price'] * $tradeSize;
+      if($btc_to_spend > 0.01)
+      {
+        $btc_to_spend = 0.01;
+        $tradeSize = $btc_to_spend / $cryptBook['asks']['price'];
+      }
+
+      print "balance: $btc_bal BTC; $balance $alt \n";
+      print "btc_to_spend = $btc_to_spend tradeSize=$tradeSize\n";
+
+      echo "Are you sure you want to do this?  Type 'yes' to continue: ";
+      $handle = fopen ("php://stdin","r");
+      $line = fgets($handle);
+      if(trim($line) != 'y'){
+          echo "ABORTING!\n";
+          continue;
+      }
+      fclose($handle);
+
+      if($btc_to_spend < $btc_bal)
+        place_limit_order($CryptopiaApi, $alt, 'buy', $cryptBook['asks']['price'], $tradeSize);
+      else
+        print "not enough BTC \n";
+
+      if($tradeSize < $balance)
+        place_limit_order($abucoinsApi, $alt, 'sell', $abuBook['bids']['price'], $tradeSize);
+      else
+        print "not enough $alt \n";
+    }
 
     sleep(2);
   }
+  print "~~~~~~~~~~~~~~cumulated profit: $profit BTC~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
 }
--- common/tools.php
+++ common/tools.php
@@ -8,7 +8,8 @@ class product
   protected $api;
 
   public $id;
-  public $min_order_size;
+  public $min_order_size_alt;
+  public $min_order_size_btc;
   public $fees;
 
   public function __construct($api, $product_id)
@@ -19,7 +20,7 @@ class product
     {
       $this->id = $product_id;
       $product = $this->api->jsonRequest('GET', "/products/{$this->id}", null);
-      $this->min_order_size = $product->base_min_size;
+      $this->min_order_size_alt = $product->base_min_size;
       $this->fees = 0; //til end of March
     }
     elseif($api instanceof CryptopiaApi)
@@ -29,8 +30,8 @@ class product
       foreach( $pairs as $pair)
         if($pair->Label == str_replace('_', '/', $this->id))
         {
-          $min_order = $pair->MinimumTrade;
-          $this->min_order_size = $pair->MinimumBaseTrade;
+          $this->min_order_size_alt = $pair->MinimumTrade;
+          $this->min_order_size_btc = $pair->MinimumBaseTrade;
           $this->fees = $pair->TradeFee;
         }
     }
@@ -63,7 +64,7 @@ class OrderBook
     {
       $book = $this->api->jsonRequest('GET', "/products/{$this->product->id}/book?level=2", null);
 
-      if(!isset($book->asks) || !isset($book->bids) || !isset($this->product->min_order_size))
+      if(!isset($book->asks) || !isset($book->bids) || !isset($this->product->min_order_size_alt))
         return null;
 
       foreach( ['asks', 'bids'] as $side)
@@ -71,7 +72,7 @@ class OrderBook
         $best[$side]['price'] = $book->$side[0][0];
         $best[$side]['size'] = $book->$side[0][1];
         $i=1;
-        while($best[$side]['size'] < $this->product->min_order_size)
+        while($best[$side]['size'] < $this->product->min_order_size_alt)
         {
           $best[$side]['price'] = ($best[$side]['price'] * $best[$side]['size'] + $book->$side[$i][0] * $book->$side[$i][1]) / 2;
           $best[$side]['size'] += $book->$side[$i][1];
@@ -80,28 +81,109 @@ class OrderBook
       }
       return $best;
     }
-    elseif($this->api instanceof CryptopiaApi)
+    elseif($this->api instanceof CryptopiaApi)//todo: Minimum total trade is 0.00050000 BTC"
     {
       $book = $this->api->jsonRequest("GetMarketOrders/{$this->product->id}/10");
 
-      if(!isset($book->Sell) || !isset($book->Buy) || !isset($this->product->min_order_size))
+      if(!isset($book->Sell) || !isset($book->Buy) || !isset($this->product->min_order_size_alt))
         return null;
 
-      foreach( ['Sell', 'Buy'] as $side)
-      {
-        $stdSide = $side == 'Buy' ? 'bids' : 'asks';
-        $best[$stdSide]['price'] = $book->$side[0]->Price;
-        $best[$stdSide]['size'] = $book->$side[0]->Volume;
+        //bids
+        $highest_bid = &$best['bids'];
+        $highest_bid['price'] = $book->Buy[0]->Price;
+        $highest_bid['size'] = $book->Buy[0]->Volume;
         $i=1;
-        while($best[$stdSide]['size'] < $this->product->min_order_size)
+        while($highest_bid['size'] * $highest_bid['price'] < $this->product->min_order_size_alt)
         {
-          $best[$stdSide]['price'] = ($best[$stdSide]['price'] * $best[$stdSide]['size'] + $book->$side[$i]->Total) / 2;
-          $best[$stdSide]['size'] += $book->$side[$i]->Volume;
+          $highest_bid['price'] = ($highest_bid['price'] * $highest_bid['size'] + $book->Buy[$i]->Total) / 2;
+          $highest_bid['size'] += $book->Buy[$i]->Volume;
           $i++;
         }
-      }
+
+        //asks
+        $lowest_ask = &$best['asks'];
+        $lowest_ask['price'] = $book->Sell[0]->Price;
+        $lowest_ask['size'] = $book->Sell[0]->Volume;
+        $i=1;
+        while($lowest_ask['size'] * $lowest_ask['price'] < $this->product->min_order_size_btc)
+        {
+          $lowest_ask['price'] = ($lowest_ask['price'] * $lowest_ask['size'] + $book->Sell[$i]->Total) / 2;
+          $lowest_ask['size'] += $book->Sell[$i]->Volume;
+          $i++;
+        }
+
       return $best;
     }
     else throw "wrong api provided";
   }
+}
+
+function save_trade($ret, $price = 0)
+{
+  print("saving trade\n");
+  //~ $new_trade = ["side" => $ret->side,
+             //~ "price" => $price ?:$ret->price,
+             //~ "size" => $ret->type == "limit" ? $ret->size : $ret->filled_size,
+             //~ "time" => $ret->created_at,
+             //~ "fees" => $ret->type == "limit" ? 0 : $ret->fill_fees,
+             //~ "id" => $ret->id,
+             //~ ];
+  //~ $tradelist = [];
+  //~ if(file_exists(TRADE_FILE))
+  //~ {
+    //~ $tradelist = json_decode(file_get_contents(TRADE_FILE));
+  //~ }
+  //~ $tradelist[] = $new_trade;
+  //~ file_put_contents(TRADE_FILE, json_encode($tradelist));
+}
+
+function place_limit_order($api, $alt, $side, $price, $volume)
+{
+  $error = '';
+  if($api instanceof AbucoinsApi)
+  {
+    $order = ['product_id' => "$alt-BTC",
+              'price'=> $price,
+              'size'=>  $volume,
+              'side'=> $side,
+              'type'=> 'limit',
+              'time_in_force' => 'IOC', // immediate or cancel
+               ];
+    var_dump($order);
+    $ret = $api->jsonRequest('POST', '/orders', $order);
+    sleep(1);
+    var_dump($ret);
+
+    if(!isset($ret->status))
+      $error = $ret->message;
+    else
+    {
+      $trade_id = $ret->id;
+    }
+  }
+  elseif($api instanceof CryptopiaApi)
+  {
+    $order = ['Market' => "$alt/BTC",
+          'Type' => $side,
+          'Rate' =>  $price,
+          'Amount' => $volume,
+           ];
+    $ret = $api->jsonRequest('SubmitTrade', $order);
+    sleep(1);
+    var_dump($ret);
+    if(!$ret->Success)
+      $error = $ret->Error;
+    else
+    {
+      $trade_id = $ret->Data->OrderId;
+    }
+  }
+
+  if ($error != '')
+  {
+    save_trade($ret, $price);
+    return $trade_id;
+  }
+  else
+    throw new Exception("order failed with status: $error\n");
 }
