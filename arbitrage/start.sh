732d12c46e4ed968b75d24a74113c6447115468b
Peterjah <petterseznec@gmail.com>
2018-03-15 19:48:42 +0100

    add screened start script

---
+++ arbitrage/start.sh
@@ -0,0 +1,4 @@
+screen -S abucoins_kraken -dm  php arbitrage.php abucoins kraken 
+screen -S abucoins_cryptopia -dm  php arbitrage.php abucoins cryptopia 
+screen -S cryptopia_kraken -dm  php arbitrage.php cryptopia kraken 
+
