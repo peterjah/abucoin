8132ca94af7d629eb2cf441fb6b5d3d8f4a96559
Peterjah <petterseznec@gmail.com>
2018-07-03 10:50:29 +0200

    Add start script tool

---
+++ arbitrage/start_screen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+exec >>looog.log 2>>looog.err.log
+set -x
+screen -S cobinhood_kraken -dm  php arbitrage.php cobinhood kraken 
+sleep 0.1
+screen -S cobinhood_binance -dm  php arbitrage.php cobinhood binance 
+sleep 0.1
+screen -S cryptopia_kraken -dm  php arbitrage.php cryptopia kraken 
+sleep 0.1
+screen -S binance_kraken -dm  php arbitrage.php binance kraken 
+sleep 0.3
+screen -S binance_cryptopia -dm  php arbitrage.php binance cryptopia 
+sleep 0.5
+screen -S cobinhood_cryptopia -dm  php arbitrage.php cobinhood cryptopia 
+
+screen -dm php clean_failed_trades.php -auto-solve
+
