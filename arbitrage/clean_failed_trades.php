347295ed64a4b465533ed177664f84e45478e8a5
Peterjah <petterseznec@gmail.com>
2018-07-24 17:13:21 +0200

    Take fees into gains account

--- arbitrage/clean_failed_trades.php
+++ arbitrage/clean_failed_trades.php
@@ -102,8 +102,25 @@ while(1)
       }
       $trade_success = false;
       if($buy_size) {
+
         print "mean_buy_price= $mean_buy_price: buy_size= $buy_size\n";
         if(@$autoSolve) {
+          //compute mean fees
+          $mean_fees=0;
+          $size = 0;
+          foreach($altOps['ops'] as $ops)
+          {
+            $Api = getMarket(strtolower($ops['exchange']));
+            $ProductInfos = $Api->getProductInfo($alt);
+            if(!$mean_fees)
+              $mean_fees = $ProductInfos['fees'];
+            else
+              $mean_fees = ($mean_fees*$size + $ProductInfos['fees']*$ops['size']) / ($size + $ops['size']);
+            $size += $ops['size'];
+          }
+          print "mean_buy_fee= $mean_fees\n";
+
+
           print "trying to solve tx..\n";
           foreach( ['binance','kraken','cobinhood','cryptopia'] as $exchange) {
             $Api = getMarket($exchange);
@@ -112,7 +129,7 @@ while(1)
                 continue;
               $orderBook = new OrderBook($Api, $alt);
               $book = $orderBook->refreshBook(0,$buy_size);
-              $Api->products[$alt] = $orderBook->product;
+              $productInfos = $Api->getProductInfo($alt);
             }catch(Exception $e) {continue;}
             if($book['bids']['price'] > $mean_buy_price)
             {
@@ -120,12 +137,16 @@ while(1)
               $i=0;
               while($i<6) {
                 try{
-                  $Api->place_order('market', $alt, 'sell', $book['bids']['order_price'], $buy_size, 'solved');
+                  $status = $Api->place_order('market', $alt, 'sell', $book['bids']['order_price'], $buy_size, 'solved');
                   foreach($altOps['ops'] as $id => $ops) {
                     if ($ops['side'] == 'buy')
                       file_put_contents(FILE, str_replace($id, 'solved', file_get_contents(FILE)));
                   }
                   $trade_success = true;
+                  $gain_btc = $sell_size * ($status['price']*((100-$productInfos['fees'])/100) - $mean_buy_price*((100+$mean_buy_fees)/100));
+                  $gain_percent = (($mean_sell_price / $book['asks']['price']) -1)*100; //nogood
+                  $trade_str = date("Y-m-d H:i:s").": $gain_btc BTC $gain_percent%\n";
+                  file_put_contents('gains',$trade_str,FILE_APPEND);
                   break;
                 } catch(Exception $e) {
                   print "failed to sell: $e \n";
@@ -143,30 +164,50 @@ while(1)
       if($sell_size) {
         print "mean_sell_price= $mean_sell_price: sell_size= $sell_size\n";
         if(@$autoSolve) {
+          //compute mean fees
+          $mean_fees=0;
+          $size = 0;
+          foreach($altOps['ops'] as $ops)
+          {
+            $Api = getMarket(strtolower($ops['exchange']));
+            $ProductInfos = $Api->getProductInfo($alt);
+            if(!$mean_fees)
+              $mean_fees = $ProductInfos['fees'];
+            else
+              $mean_fees = ($mean_fees*$size + $ProductInfos['fees']*$ops['size']) / ($size + $ops['size']);
+            $size += $ops['size'];
+          }
+          print "mean_buy_fee= $mean_fees\n";
+
           print "trying to solve tx..\n";
           foreach( ['binance','kraken','cobinhood','cryptopia'] as $exchange) {
             $Api = getMarket($exchange);
             try {
-              if($Api->getBalance('BTC') < $sell_size * $book['asks']['order_price'])
-               continue;
                $orderBook = new OrderBook($Api, $alt);
                $book = $orderBook->refreshBook(0,$sell_size);
-               $Api->products[$alt] = $orderBook->product;
+               if($Api->getBalance('BTC') < $sell_size * $book['asks']['order_price'])
+                continue;
+               $productInfos = $Api->getProductInfo($alt);
             }catch(Exception $e) {continue;}
+
             print " {$Api->name} asks order price: {$book['asks']['order_price']}";
-            if($book['asks']['price'] < $mean_sell_price)
+            if($book != null && $book['asks']['price'] < $mean_sell_price)
             {
               var_dump($book['asks']);
               $i=0;
               while($i<6) {
                 try{
                   print "place order price: {$book['asks']['order_price']}";
-                  $Api->place_order('market', $alt, 'buy', $book['asks']['order_price'], $sell_size, 'solved');
+                  $status = $Api->place_order('market', $alt, 'buy', $book['asks']['order_price'], $sell_size, 'solved');
                   foreach($altOps['ops'] as $id => $ops) {
                     if ($ops['side'] == 'sell')
                       file_put_contents(FILE, str_replace($id, 'solved', file_get_contents(FILE)));
                     }
                   $trade_success = true;
+                  $gain_btc = $sell_size * ($mean_sell_price*((100-$mean_fees)/100) - $status['price']*((100+$productInfos['fees'])/100));
+                  $gain_percent = (($mean_sell_price / $book['asks']['price']) -1)*100; //nogood
+                  $trade_str = date("Y-m-d H:i:s").": $gain_btc BTC $gain_percent%\n";
+                  file_put_contents('gains',$trade_str,FILE_APPEND);
                   break;
                 } catch(Exception $e) {
                   print "failed to buy: $e \n";
