cf6b089a26345d91d00430da0c17aa8f4f896ad3
Peterjah <petterseznec@gmail.com>
2019-01-04 21:41:13 +0100

    New swap policy

--- arbitrage/arbitrage.php
+++ arbitrage/arbitrage.php
@@ -17,8 +17,8 @@ pcntl_signal(SIGTERM, "sig_handler");
 $sig_stop = false;
 
 @define('BUY_TRESHOLD', 0.000001);
-@define('CRITICAL_BUY_TRESHOLD', -0.000005);
-@define('CRITICAL_BUY_TRESHOLD2', -0.00001);
+@define('CRITICAL_BUY_TRESHOLD_BASE', -0.000001);
+@define('CRITICAL_BUY_TRESHOLD_MAX', 0.00001);
 
 $market1 = new Market($argv[1]);
 $market2 = new Market($argv[2]);
@@ -132,9 +132,12 @@ function testSwap($symbol, $buy_market, $sell_market)
   $alt = $buy_product->alt;
   $base = $sell_product->base;
   while($final_gains['base'] > 0) {
+
+    $final_gains['base'] = 0;
     $base_cash_roll = $buy_market->api->balances[$base] + $sell_market->api->balances[$base];
     $get_base_market = $buy_market->api->balances[$base] > $sell_market->api->balances[$base];
     $get_base_market_critical = $base_cash_roll > 0.001 ? $sell_market->api->balances[$base] < $base_cash_roll * 0.1 /*10% of cashroll*/: false;
+    $critical_treshold = CRITICAL_BUY_TRESHOLD_BASE;
 
     $min_order_size_base = max($buy_product->min_order_size_base, $sell_product->min_order_size_base);
     $min_order_size_alt = max($buy_product->min_order_size, $sell_product->min_order_size);
@@ -147,63 +150,75 @@ function testSwap($symbol, $buy_market, $sell_market)
     $sell_book = $sell_market->refreshBook($sell_product, $min_order_size_base, $min_order_size_alt);
 
     $sell_price = $sell_book['bids']['price'];
+    $sell_order_price = $sell_book['bids']['order_price'];
     $buy_price = $buy_book['asks']['price'];
-    $tradeSize = min($sell_book['bids']['size'], $buy_book['asks']['size']);
-    print("tradesize $tradeSize\n");
+    $buy_order_price = $buy_book['asks']['order_price'];
+    $trade_size = min($sell_book['bids']['size'], $buy_book['asks']['size']);
+
     $buy_fees = $buy_product->fees;
     $sell_fees = $sell_product->fees;
 
-    $expected_gains = computeGains($buy_price, $buy_fees, $sell_price, $sell_fees, $tradeSize);
-    //swap conditions
-    $do_swap = false;
-    if($base == 'BTC') {
-      if ($expected_gains['base'] > BUY_TRESHOLD ||
-         ($get_base_market_critical && ($expected_gains['base'] >= CRITICAL_BUY_TRESHOLD)) ||
-         ($get_base_market && $expected_gains['base'] >= 0) ) {
-           $do_swap = true;
-         }
-    } else if ($expected_gains['base'] > 0) {
-      $do_swap = true;
-    }
+    $trade_size = check_tradesize($symbol, $sell_market, $sell_order_price, $buy_market, $buy_order_price, $trade_size);
+    if ($trade_size > 0) {
+      $expected_gains = computeGains($buy_price, $buy_fees, $sell_price, $sell_fees, $trade_size);
+      //swap conditions
+      $do_swap = false;
+      if($base == 'BTC') {
+        //compute critical treshold
+        //avoid swap to big when gain is <0
+        if ($get_base_market_critical) {
+          $half_cash = $base_cash_roll / 2;
+          if ($expected_gains['base'] < 0) {
+            $tmptradesize = $trade_size;
+            $trade_size = min($trade_size, $half_cash / $sell_price);
+            $trade_size = check_tradesize($symbol, $sell_market, $sell_order_price, $buy_market, $buy_order_price, $trade_size);
+            $multiplier = ($trade_size *$sell_price) / $half_cash;
+            $critical_treshold = CRITICAL_BUY_TRESHOLD_BASE * (1 + 10 * $multiplier);
+          }
+        }
+        $critical_tresh = $trade_size * $sell_size / $half_cash;
+        if ($expected_gains['base'] > BUY_TRESHOLD ||
+           ($get_base_market_critical && ($expected_gains['base'] >= $critical_treshold)) ||
+           ($get_base_market && ($expected_gains['base'] >= 0)) ) {
+             $do_swap = true;
+           }
+      } else if ($expected_gains['base'] > 0) {
+        $do_swap = true;
+      }
 
-    if ($do_swap) {
-      //avoid swap to big when gain is <0
-      if ($get_base_market_critical && $base == 'BTC') {
-        $half_cash = $base_cash_roll / 2;
-        if ((($tradeSize * $sell_price) > $half_cash) && $expected_gains['base'] < 0) {
-          $new_tradeSize = $half_cash / $sell_price > $min_order_size_alt ? $half_cash / $sell_price: $min_order_size_alt;
-          print_dbg("Critical swap: reducing tradesize from $tradeSize to $new_tradeSize $alt", true);
-          $tradeSize = $new_tradeSize;
+      // if($base == 'BTC' && $expected_gains['base'] < 0 && $get_base_market_critical) {
+      //   $factor = (($trade_size *$sell_price) / $half_cash );
+      //   print_dbg("Critical swap: reducing tradesize from $tmptradesize to $trade_size $alt", true);
+      //   print_dbg("Critical tresh: $critical_treshold => swap ".(($trade_size *$sell_price*100) / $half_cash)."% multiplier: $factor", true);
+      //   print_dbg("Critical do_swap: ".($do_swap?'yes':'no')." expected gains: {$expected_gains['base']}" , true);
+      //
+      // }
+      if ($do_swap) {
+        $buy_market->getBalance();
+        $sell_market->getBalance();
+
+        print "do arbitrage for {$symbol}. estimated gain: ".number_format($expected_gains['percent'], 3)."%";
+        $status = do_arbitrage($symbol, $sell_market, $sell_order_price, $buy_market, $buy_order_price, $trade_size);
+        if ($status['buy']['filled_size'] > 0 && $status['sell']['filled_size'] > 0) {
+
+          if ($status['buy']['filled_size'] != $status['sell']['filled_size'])
+            print_dbg("Different tradesizes buy:{$status['buy']['filled_size']} != sell:{$status['sell']['filled_size']}");
+
+          $trade_size = min($status['buy']['filled_size'] , $status['sell']['filled_size']);
+          $final_gains = computeGains($status['buy']['price'], $buy_fees, $status['sell']['price'], $sell_fees, $trade_size);
+          $profit += $final_gains['base'];
+          print("log tx\n");
+          $trade_str = date("Y-m-d H:i:s").": {$final_gains['base']} $base {$expected_gains['percent']}% ({$final_gains['percent']}%)\n";
+          file_put_contents('gains',$trade_str,FILE_APPEND);
+
+          //Just in case
+          $buy_market->api->balances[$alt] += $status['buy']['filled_size'];
+          $buy_market->api->balances[$base] -= $trade_size * $status['sell']['price'];
+          $sell_market->api->balances[$base] += $trade_size * $status['buy']['price'];
+          $sell_market->api->balances[$alt] -= $status['sell']['filled_size'];
         }
       }
-      $buy_market->getBalance();
-      $sell_market->getBalance();
-
-      print "do arbitrage for {$symbol}. estimated gain: ".number_format($expected_gains['percent'], 3)."%";
-      $status = do_arbitrage($symbol, $sell_market, $sell_book['bids']['order_price'], $buy_market, $buy_book['asks']['order_price'], $tradeSize);
-      if ($status['buy']['filled_size'] > 0 && $status['sell']['filled_size'] > 0) {
-
-        if ($status['buy']['filled_size'] != $status['sell']['filled_size'])
-          print_dbg("Different tradesizes buy:{$status['buy']['filled_size']} != sell:{$status['sell']['filled_size']}");
-
-        $tradeSize = min($status['buy']['filled_size'] , $status['sell']['filled_size']);
-        $final_gains = computeGains($status['buy']['price'], $buy_fees, $status['sell']['price'], $sell_fees, $tradeSize);
-        $profit += $final_gains['base'];
-        print("log tx\n");
-        $trade_str = date("Y-m-d H:i:s").": {$final_gains['base']} $base {$expected_gains['percent']}% ({$final_gains['percent']}%)\n";
-        file_put_contents('gains',$trade_str,FILE_APPEND);
-
-        //Just in case
-        $buy_market->api->balances[$alt] += $status['buy']['filled_size'];
-        $buy_market->api->balances[$base] -= $tradeSize * $status['sell']['price'];
-        $sell_market->api->balances[$base] += $tradeSize * $status['buy']['price'];
-        $sell_market->api->balances[$alt] -= $status['sell']['filled_size'];
-      }
-      else
-        $final_gains['base'] = 0;
     }
-    else
-        $final_gains['base'] = 0;
   }
   return $profit;
 }
