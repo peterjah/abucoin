f5d54e01944c8fec0a40a84e306248959f59dc83
peterjah <petterseznec@gmail.com>
2019-01-09 00:03:20 +0100

    New multichange algo. WIP

--- multichange/multichange.php
+++ multichange/multichange.php
@@ -1,98 +1,159 @@
 <?php
-require_once('../common/abucoin_api.php');
-require_once('../common/cryptopia_api.php');
+require_once('../common/tools.php');
 
-$keys = json_decode(file_get_contents("../common/private.keys"));
-$abucoinsApi = new AbucoinsApi($keys->abucoins);
-$CryptopiaApi = new CryptopiaApi($keys->cryptopia);
-
-function getOrderBook($abucoinsApi, $product_id)
-{
-  $product = $abucoinsApi->jsonRequest('GET', "/products/$product_id", null);
-  $book = $abucoinsApi->jsonRequest('GET', "/products/$product_id/book?level=2", null);
-
-  if(!isset($book->asks) || !isset($book->bids) || !isset($product->base_min_size))
-   return null;
-
-  foreach( ['asks', 'bids'] as $side)
-  {
-    $best[$side]['price'] = $book->$side[0][0];
-    $best[$side]['size'] = $book->$side[0][1];
-    $i=1;
-    while($best[$side]['size'] < $product->base_min_size)
-    {
-      $best[$side]['price'] = ($best[$side]['price'] * $best[$side]['size'] + $book->$side[$i][0] * $book->$side[$i][1]) / 2;
-      $best[$side]['size'] += $book->$side[$i][1];
+//init api
+$markets = [];
+$products = [];
+foreach( ['binance','kraken','cobinhood','cryptopia'] as $name) {
+  $i=0;
+  while($i<6) {
+    try{
+      $markets[$name] = new Market($name);
+      $markets[$name]->api->getBalance();
+      foreach($markets[$name]->products as $product)
+        $products[] = $product;
+      break;
+    } catch(Exception $e) {
+      print "failed to get market $name: $e \n";
+      usleep(500000);
       $i++;
     }
   }
-  return $best;
 }
-
-$ntrades = 0;
-while(true)
-{
-
-  foreach(['EUR', 'USD', 'PLN' ] as $Fiat)
-  {
-    print("Try multi switch with $Fiat\n");
-    $account = $abucoinsApi->jsonRequest('GET', "/accounts/10502694-$Fiat", null);
-    if(!isset($account->available))
+  //iterate over all products
+foreach ($products as $product1) {
+  foreach ($products as $product2) {
+    if ($product1 == $product2)
       continue;
-    $balance = $account->available;
-    print("$Fiat balance = $balance\n");
-    //FIAT -> BTC -> Crypto -> FIAT
-    //Get order book
-
-    foreach(['LSK', 'ETH', 'BCH' , 'BTG'] as $Crypto)
-    {
-      print("Try multi switch with $Crypto\n");
-      if( ($BTC_FIATbook = getOrderBook($abucoinsApi, "BTC-$Fiat")) == null ||
-          ($Crypto_BTCbook = getOrderBook($abucoinsApi, "$Crypto-BTC"))== null ||
-          ($Crypto_FIATbook = getOrderBook($abucoinsApi, "$Crypto-$Fiat"))== null)
+    //case buy product 1
+    if ($product1->alt == $product2->base){
+      if ($product1->base == $product2->alt)
         continue;
+      // ETH-BTC REP-ETH REP-BTC
+      // BTC->ETH ETH->REP REP->BTC
+      $quote = $product1->base;
+      $swap1 = $product1->alt;
+      $swap2 = $product2->alt;
+      $action1 = 'buy';
+      $action2 = 'buy';
+      $symbol3 = "{$swap2}-{$quote}";
+      $action3 = 'sell';
+      foreach ($products as $product3) {
+        if ($product3->symbol == $symbol3) {
+          $book1 = $product1->refreshBook()['bids'];
+          $sizequote = $product1->api->balances[$quote];
+          $sizeswap1 = ($sizequote / $book1['order_price']) * (1 - $product1->fees/100);
+          $book2 = $product2->refreshBook()['bids'];
+          $sizeswap2 = ($sizeswap1 / $book2['order_price']) * (1 - $product2->fees/100);
 
-      $bestBTC_Seller = $BTC_FIATbook['asks'];//ACHTUNG bids<->asks //var_dump($bestBTC_Seller);
-      $bestCrypto_Seller = $Crypto_BTCbook['asks']; //var_dump($bestCrypto_Seller);
-      $bestCrypto_Buyer = $Crypto_FIATbook['bids']; //var_dump($bestCrypto_Buyer);
-
-      $roundAmountEur = $bestBTC_Seller['price'] * $bestBTC_Seller['size'];
-      if( ($bestCrypto_Seller['price'] *$bestCrypto_Seller['size']) < $bestBTC_Seller['size'])
-      {
-        $btc2buy = $bestCrypto_Seller['price'] *$bestCrypto_Seller['size'];
-        $roundAmountEur = $btc2buy * $bestBTC_Seller['price'];
-        //print("wesh1 $roundAmountEur\n");
+          $book3 = $product3->refreshBook()['asks'];
+          $sizequotefinal = $sizeswap2 * $book3['order_price'] * (1 - $product3->fees/100);
+          if ($sizequotefinal > $sizequote) {
+            print "$quote->$swap1 ({$product1->api->name}) "
+                  ."$swap1->$swap2 ({$product2->api->name}) "
+                  ."$swap2->$quote ({$product3->api->name})\n";
+            $gain = $sizequotefinal - $sizequote;
+            $gainpercent = number_format(($gain / $sizequote)*100, 2);;
+            print "YAAAHOUUUU sizequote: $sizequote $quote gains: ".$gain." $quote $gainpercent%\n";
+          }
+        }
       }
-      if($bestCrypto_Buyer['price'] * $bestCrypto_Buyer['size'] < $roundAmountEur)
-      {
-        $roundAmountEur = $bestCrypto_Buyer['price'] * $bestCrypto_Buyer['size'];
-        //print("wesh2 $roundAmountEur\n");
+    } elseif ($product1->alt == $product2->alt) {
+      if ($product1->base == $product2->base)
+        continue;
+      // ETH-BTC ETH-REP BTC-REP
+      // BTC->ETH ETH->REP REP->BTC
+      $quote = $product1->base;
+      $swap1 = $product1->alt;
+      $swap2 = $product2->alt;
+      $action1 = 'buy';
+      $action2 = 'sell';
+      $symbol3 = "{$quote}-{$swap2}";
+      $action3 = 'buy';
+      foreach ($products as $product3) {
+        if ($product3->symbol == $symbol3) {
+          $book1 = $product1->refreshBook()['bids'];
+          $sizequote = $product1->api->balances[$quote];
+          $sizeswap1 = ($sizequote / $book1['order_price']) * (1 - $product1->fees/100);
+          $book2 = $product2->refreshBook()['asks'];
+          $sizeswap2 = $sizeswap1 * $book2['order_price'] * (1 - $product2->fees/100);
+
+          $book3 = $product3->refreshBook()['bids'];
+          $sizequotefinal = ($sizeswap2 / $book3['order_price']) * (1 - $product3->fees/100);
+          if ($sizequotefinal > $sizequote) {
+            print "$quote->$swap1 ({$product1->api->name}) "
+                  ."$swap1->$swap2 ({$product2->api->name}) "
+                  ."$swap2->$quote ({$product3->api->name})\n";
+            $gain = $sizequotefinal - $sizequote;
+            $gainpercent = number_format(($gain / $sizequote)*100, 2);;
+            print "YAAAHOUUUU sizequote: $sizequote $quote gains: ".$gain." $quote $gainpercent%\n";
+          }
+        }
       }
+    } elseif ($product1->base == $product2->base) {
+      if ($product1->alt == $product2->alt)
+        continue;
+      // ETH-BTC REP-BTC REP-ETH
+      // ETH->BTC BTC->REP REP->ETH
+      $quote = $product1->alt;
+      $swap1 = $product1->base;
+      $swap2 = $product2->alt;
+      $action1 = 'sell';
+      $action2 = 'buy';
+      $symbol3 = "{$swap2}-{$quote}";
+      $action3 = 'sell';
+      foreach ($products as $product3) {
+        if ($product3->symbol == $symbol3) {
+          $book1 = $product1->refreshBook()['asks'];
+          $sizequote = $product1->api->balances[$quote];
+          $sizeswap1 = $sizequote * $book1['order_price'] * (1 - $product1->fees/100);
+          $book2 = $product2->refreshBook()['bids'];
+          $sizeswap2 = ($sizeswap1 / $book2['order_price']) * (1 - $product2->fees/100);
 
-  //    if($roundAmountEur >= $balance)
-  //      $roundAmountEur = $balance;
-      print("play with = $roundAmountEur $Fiat\n");
-
-      //buy $roundAmountEur of btc
-      $tmpBtc = $roundAmountEur / $bestBTC_Seller['price'];
-        //var_dump("tmpBtc = $tmpBtc");
-      //buy $tmpBtc of lsk
-      $tmpLsk = $tmpBtc / $bestCrypto_Seller['price'];
-        //var_dump("tmpLsk = $tmpLsk");
-      //sell lsk for eur
-      $gainEur = $tmpLsk * $bestCrypto_Buyer['price'] - $roundAmountEur;
-      $gainPercent = ($gainEur/$roundAmountEur)*100;
-      print("gain = ".number_format($gainEur,2)." Eur ".number_format($gainPercent, 2) ."%\n");
-
-
-      if($gainEur > 0.1) //10 cts!
-      { // do trades
-        exec("$ntrades - echo playing with $roundAmountEur $Fiat in $Crypto gain $gainEur $Fiat = $gainPercent%>> gains");
-        exec("$ntrades - echo buy $tmpBtcBTC at {$bestBTC_Seller['price']}; buy $tmpLsk at {$bestCrypto_Seller['price']}; sell $tmpLsk at {$bestCrypto_Buyer['price']}>> debug");
+          $book3 = $product3->refreshBook()['asks'];
+          $sizequotefinal = $sizeswap2 * $book3['order_price'] * (1 - $product3->fees/100);
+          if ($sizequotefinal > $sizequote) {
+            print "$quote->$swap1 ({$product1->api->name}) "
+                  ."$swap1->$swap2 ({$product2->api->name}) "
+                  ."$swap2->$quote ({$product3->api->name})\n";
+            $gain = $sizequotefinal - $sizequote;
+            $gainpercent = number_format(($gain / $sizequote)*100, 2);;
+            print "YAAAHOUUUU sizequote: $sizequote $quote gains: ".$gain." $quote $gainpercent%\n";
+          }
+        }
       }
-      print("\n");
-    }
+    }  elseif ($product1->base == $product2->alt) {
+      if ($product1->alt == $product2->base)
+        continue;
+      // ETH-BTC BTC-REP ETH-REP
+      // ETH->BTC BTC->REP REP->ETH
+      $quote = $product1->alt;
+      $swap1 = $product1->base;
+      $swap2 = $product2->alt;
+      $action1 = 'sell';
+      $action2 = 'sell';
+      $symbol3 = "{$quote}-{$swap2}";
+      $action3 = 'buy';
+      foreach ($products as $product3) {
+        if ($product3->symbol == $symbol3) {
+          $book1 = $product1->refreshBook()['asks'];
+          $sizequote = $product1->api->balances[$quote];
+          $sizeswap1 = $sizequote * $book1['order_price'] * (1 - $product1->fees/100);
+          $book2 = $product2->refreshBook()['asks'];
+          $sizeswap2 = $sizeswap1 * $book2['order_price'] * (1 - $product2->fees/100);
+
+          $book3 = $product3->refreshBook()['bids'];
+          $sizequotefinal = ($sizeswap2 / $book3['order_price']) * (1 - $product3->fees/100);
+          if ($sizequotefinal > $sizequote) {
+            print "$quote->$swap1 ({$product1->api->name}) "
+                  ."$swap1->$swap2 ({$product2->api->name}) "
+                  ."$swap2->$quote ({$product3->api->name})\n";
+            $gain = $sizequotefinal - $sizequote;
+            $gainpercent = number_format(($gain / $sizequote)*100, 2);;
+            print "YAAAHOUUUU sizequote: $sizequote $quote gains: ".$gain." $quote $gainpercent%\n";
+          }
+        }
+      }
+    } else continue;
   }
-  print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
-  sleep(5);
 }
