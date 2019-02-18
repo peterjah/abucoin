6436cd9aaffde354d5f1fbd51587255750bf2c71
Peterjah <petterseznec@gmail.com>
2019-02-18 16:08:21 +0100

    Add mean percent gains

--- arbitrage/gains.php
+++ arbitrage/gains.php
@@ -7,11 +7,12 @@ $options =  getopt('', array(
 
 $handle = fopen("gains", "r");
 $gains = [];
-
+$nTx = [];
 $prices['EUR'] = 1;
 foreach (['BTC','ETH','USDT'] as $base) {
   $price = json_decode(file_get_contents("https://min-api.cryptocompare.com/data/price?fsym={$base}&tsyms=EUR"), true);
   $prices[$base] = $price['EUR'];
+  $nTx[$base] = 0;
 }
 $prices['USD'] = $prices['USDT'];
 
@@ -30,27 +31,37 @@ if ($handle) {
         }
         $gains_base = $matches[3];
         print "{$matches[1]}: {$exchanges} ".sprintf("%.3e",$gains_base)." {$base} =".number_format($gains_base*$prices[$base], 3)."EUR ".number_format($matches[5], 2)."% ($real_percent_gains%)\n";
-        if (isset($options['exchange1']) && $exchanges != '') {
+        if (isset($options['exchange1']) ) {
           $exchange1 = strtolower(substr($options['exchange1'], 0, 2));
           if(strpos($exchanges, $exchange1) !== false) {
             @$gains[$exchange1][$base] += floatval($gains_base);
+            @$mean_percent_gains[$exchange1][$base] += $real_percent_gains;
+            $nTx[$base]++;
           }
         }
-        else
-          @$gains[$base] += floatval($gains_base);
+        else {
+         @$gains[$base] += floatval($gains_base);
+         @$mean_percent_gains[$base] += $real_percent_gains;
+         $nTx[$base]++;
+        }
     }
     fclose($handle);
 }
 print "\n";
 $total_gains_eur = 0;
-if (isset($options['exchange1'])) {
-  $gains = @$gains[$exchange1];
+if (isset($options['exchange1']) && !empty($gains[$exchange1])) {
+  print "~~~~~~~~~~~~~~~~ {$options['exchange1']} ~~~~~~~~~~~~~~~~~~~\n";
+  $gains = $gains[$exchange1];
+  $mean_percent_gains = $mean_percent_gains[$exchange1];
 }
 
 if (isset($gains)) {
   foreach ($gains as $base => $gain) {
+    $mean_percent_gains = $mean_percent_gains[$base] / $nTx[$base];
     $total_gains_eur += $gain * $prices[$base];
+    print "~~~~~~~~~~~~~~~~ $base ~~~~~~~~~~~~~~~~~~~\n";
     print ("gains: $gain $base = ".($gain * $prices[$base])."EUR\n");
+    print ("mean gains: $mean_percent_gains %\n");
   }
 }
 print ("total: $total_gains_eur EUR\n");
