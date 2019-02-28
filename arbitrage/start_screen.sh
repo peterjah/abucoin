ceb918f55901bd553be2d668e997220b9fef333c
Peterjah <petterseznec@gmail.com>
2019-02-28 17:47:03 +0100

    Update start_screen.sh

--- arbitrage/start_screen.sh
+++ arbitrage/start_screen.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-cd /home/pseznec/dev/abucoin/abucoin/arbitrage/
-screen -S cobinhood_kraken -dm  php arbitrage.php cobinhood kraken 
+cd $PWD
+screen -S cobinhood_kraken -dm  php arbitrage.php cobinhood kraken
 sleep 0.1
-screen -S cobinhood_binance -dm  php arbitrage.php cobinhood binance 
+screen -S cobinhood_binance -dm  php arbitrage.php cobinhood binance
 sleep 0.1
-screen -S cryptopia_kraken -dm  php arbitrage.php cryptopia kraken 
+screen -S cryptopia_kraken -dm  php arbitrage.php cryptopia kraken
 sleep 0.1
-screen -S binance_kraken -dm  php arbitrage.php binance kraken 
+screen -S binance_kraken -dm  php arbitrage.php binance kraken
 sleep 0.3
-screen -S binance_cryptopia -dm  php arbitrage.php binance cryptopia 
+screen -S binance_cryptopia -dm  php arbitrage.php binance cryptopia
 sleep 0.5
-screen -S cobinhood_cryptopia -dm  php arbitrage.php cobinhood cryptopia 
+screen -S cobinhood_cryptopia -dm  php arbitrage.php cobinhood cryptopia
+sleep 0.5
+screen -S paymium_kraken -dm  php arbitrage.php paymium kraken
 
 screen -S trade_cleaner -dm php clean_failed_trades.php -auto-solve
-
