fe719f4a76e1ea98d8e959c91f60fe26df902656
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-03-12 20:52:54 +0100

    update start script

--- arbitrage/start_screen.sh
+++ arbitrage/start_screen.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 cd $PWD
-screen -S cobinhood_kraken -dm  php arbitrage.php cobinhood kraken
-sleep 0.1
-screen -S cobinhood_binance -dm  php arbitrage.php cobinhood binance
-sleep 0.1
-screen -S cryptopia_kraken -dm  php arbitrage.php cryptopia kraken
-sleep 0.1
 screen -S binance_kraken -dm  php arbitrage.php binance kraken
-sleep 0.3
-screen -S binance_cryptopia -dm  php arbitrage.php binance cryptopia
-sleep 0.5
-screen -S cobinhood_cryptopia -dm  php arbitrage.php cobinhood cryptopia
-sleep 0.5
-screen -S paymium_kraken -dm  php arbitrage.php paymium kraken
 
 screen -S trade_cleaner -dm php clean_failed_trades.php -auto-solve
