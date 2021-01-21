b18ae1ee2a20588c48c9458d5b51128c6106b08d
Pierre Seznec <pierre.seznec@spacelephant.org>
2021-01-21 12:37:44 +0100

    Cleaner: set multi for cleanerTx trades

--- arbitrage/clean_failed_trades.php
+++ arbitrage/clean_failed_trades.php
@@ -202,7 +202,7 @@ function do_solve($markets, $symbol, $side, $traded)
             $leftSize = $traded['size'] - $status['filled_size'];
             if ($leftSize > 1E-8) {
                 print_dbg("partial solved, creating tosolve trade", true);
-                save_trade($traded['exchange'], $product->alt, $product->base, $side, $leftSize, $traded['price'], "partialSolve");
+                save_trade("multi", $product->alt, $product->base, $side, $leftSize, $traded['price'], "partialSolve");
             }
             // update balances
             if($side === "buy") {
@@ -272,8 +272,7 @@ function firstPassSolve($traded)
             $alt = $op[0]['alt'];
             $base = $op[0]['base'];
             if ($res_size > 0) {
-                $exchange = $res_side === 'buy' ? $buy['exchange'] : $sell['exchange'];
-                save_trade($exchange, $alt, $base, $res_side, $res_size, $res_price, "toSolve");
+                save_trade("multi", $alt, $base, $res_side, $res_size, $res_price, "toSolve");
             }
             $arbitrage_log = [ 'date' => date("Y-m-d H:i:s"),
                         'alt' => $alt,
